7bd54785057ba9d08fe94cdb87ce0731b98b8b60
dgormez <dgormez@gmail.com>
2016-12-12 15:29:44 +0100

    Works for conversion < 19

---
+++ Decimal_Roman_Convertor/RomanNumeral.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_Roman_Convertor
+{
+    public class RomanNumeral
+    {
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public string convertTo(int input)
+        {
+            string output = string.Empty;
+
+            if (input > 9)
+            {
+                output += "X";
+                input -= 10;
+            }
+
+            if (input == 9)
+            {
+                output += "IX";
+                return output;
+            }
+
+
+            if (input > 4)
+            {
+                output += "V";
+                input -= 5;
+            }
+
+            if (input == 4)
+            {
+                output += "IV";
+                return output;
+            }
+
+            //input = Math.Abs(input);
+            
+            output += convertToI(input);
+            
+            //The number was build in reverse order. Print it correct way.
+            //output = reverseString(output);
+
+            return output;
+        }
+
+        public string convertToI(int input)
+        {
+            if (input < 0)
+                return string.Empty;
+
+            string output = string.Empty;
+
+            for (int i = 0; i < input; ++i)
+                output = "I" + output;
+
+            return output;
+        }
+
+        public string reverseString(string stringToReverse)
+        {
+            if (stringToReverse == "")
+                return string.Empty;
+    
+            char[] arr = stringToReverse.ToCharArray();
+	        Array.Reverse(arr);
+
+	        return new string(arr);
+        }
+    }
+}
---
+++ RomanNumerals_Tests/RomanNumeralTests.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Decimal_Roman_Convertor
+{
+    [TestClass]
+    public class RomanNumeralTests
+    {
+        private RomanNumeral DeciToRomanConvertor;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            DeciToRomanConvertor = new RomanNumeral();
+        }
+
+        [TestCleanup]
+        public void Testcleanup()
+        {
+            DeciToRomanConvertor.Dispose();
+        }
+
+        [TestMethod]
+        public void convertTo_zero_empty()
+        {
+            int input = 0;
+            string expected = "";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_1_I()
+        {
+            int input = 1;
+            string expected = "I";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_2_II()
+        {
+            int input = 2;
+            string expected = "II";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void convertTo_3_III()
+        {
+            int input = 3;
+            string expected = "III";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_4_IV()
+        {
+            int input = 4;
+            string expected = "IV";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_5_V()
+        {
+            int input = 5;
+            string expected = "V";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_6_VI()
+        {
+            int input = 6;
+            string expected = "VI";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_7_VII()
+        {
+            int input = 7;
+            string expected = "VII";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_8_VIII()
+        {
+            int input = 8;
+            string expected = "VIII";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_9_IX()
+        {
+            int input = 9;
+            string expected = "IX";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_10_X()
+        {
+            int input = 10;
+            string expected = "X";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_11_XI()
+        {
+            int input = 11;
+            string expected = "XI";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_14_XIV()
+        {
+            int input = 14;
+            string expected = "XIV";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_16_XVI()
+        {
+            int input = 16;
+            string expected = "XVI";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void convertTo_19_XIX()
+        {
+            int input = 19;
+            string expected = "XIX";
+
+            string actual = DeciToRomanConvertor.convertTo(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        #region Helpers
+        [TestMethod]
+        public void reverseString_Imlegend_dnegelmI()
+        {
+            string input = "Imlegend";
+            string expected = "dnegelmI";
+
+            string actual = DeciToRomanConvertor.reverseString(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+    }
+}
