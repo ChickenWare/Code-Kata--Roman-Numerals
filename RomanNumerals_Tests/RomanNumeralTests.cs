8b8c1e1729a5dab944673da055ac05b32baaeb2e
dgormez <dgormez@gmail.com>
2016-12-13 17:26:11 +0100

    Updated tests

--- RomanNumerals_Tests/RomanNumeralTests.cs
+++ RomanNumerals_Tests/RomanNumeralTests.cs
@@ -20,12 +20,48 @@ namespace Decimal_Roman_Convertor
             DeciToRomanConvertor.Dispose();
         }
 
-        #region passed
+
         [TestMethod]
-        public void convertTo_zero_empty()
+        public void convertTo_OTo20_OTo20()
         {
-            int input = 0;
-            string expected = "";
+            int[] input = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+            string[] expected = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX"};
+
+            for (int i = 0; i < expected.Length - 1; i++)
+            {
+                string actual = DeciToRomanConvertor.convertTo(input[i]);
+                Assert.AreEqual(expected[i], actual);
+            } 
+        }
+
+        [TestMethod]
+        public void convertTo_Random_Random()
+        {
+            int[] input = { 50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,
+                              70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,
+                              89,90,91,92,93,94,95,96,97,98,99,100,200,300,400,500,600,700,
+                              800,900};
+            string[] expected = {"L","LI","LII","LIII","LIV","LV","LVI","LVII","LVIII","LIX",
+                                    "LX","LXI","LXII","LXIII","LXIV","LXV","LXVI","LXVII","LXVIII",
+                                    "LXIX","LXX","LXXI","LXXII","LXXIII","LXXIV","LXXV","LXXVI","LXXVII",
+                                    "LXXVIII","LXXIX","LXXX","LXXXI","LXXXII","LXXXIII","LXXXIV",
+                                    "LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC","XCI","XCII",
+                                    "XCIII","XCIV","XCV","XCVI","XCVII","XCVIII","XCIX","C","CC","CCC",
+                                    "CD","D","DC","DCC","DCCC","CM" };
+
+            for (int i = 0; i < expected.Length - 1; i++)
+            {
+                string actual = DeciToRomanConvertor.convertTo(input[i]);
+                Assert.AreEqual(expected[i], actual);
+            }
+        }
+
+        
+        [TestMethod]
+        public void convertTo_34_XXXIV()
+        {
+            int input = 34;
+            string expected = "XXXIV";
 
             string actual = DeciToRomanConvertor.convertTo(input);
 
@@ -33,166 +69,10 @@ namespace Decimal_Roman_Convertor
         }
 
         [TestMethod]
-        public void convertTo_1_I()
+        public void convertTo_35_XXXV()
         {
-            int input = 1;
-            string expected = "I";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_2_II()
-        {
-            int input = 2;
-            string expected = "II";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [TestMethod]
-        public void convertTo_3_III()
-        {
-            int input = 3;
-            string expected = "III";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_4_IV()
-        {
-            int input = 4;
-            string expected = "IV";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_5_V()
-        {
-            int input = 5;
-            string expected = "V";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_6_VI()
-        {
-            int input = 6;
-            string expected = "VI";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_7_VII()
-        {
-            int input = 7;
-            string expected = "VII";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_8_VIII()
-        {
-            int input = 8;
-            string expected = "VIII";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_9_IX()
-        {
-            int input = 9;
-            string expected = "IX";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_10_X()
-        {
-            int input = 10;
-            string expected = "X";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_11_XI()
-        {
-            int input = 11;
-            string expected = "XI";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_14_XIV()
-        {
-            int input = 14;
-            string expected = "XIV";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_15_XV()
-        {
-            int input = 15;
-            string expected = "XV";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-
-        [TestMethod]
-        public void convertTo_16_XVI()
-        {
-            int input = 16;
-            string expected = "XVI";
-
-            string actual = DeciToRomanConvertor.convertTo(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void convertTo_19_XIX()
-        {
-            int input = 19;
-            string expected = "XIX";
+            int input = 35;
+            string expected = "XXXV";
 
             string actual = DeciToRomanConvertor.convertTo(input);
 
@@ -220,8 +100,6 @@ namespace Decimal_Roman_Convertor
 
             Assert.AreEqual(expected, actual);
         }
-
-        #endregion
 
         [TestMethod]
         public void convertTo_44_XLIV()
